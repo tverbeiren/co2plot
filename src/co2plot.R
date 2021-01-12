51298ce0f3eff75b24ded324d82853732aef7b15
Robrecht Cannoodt <rcannood@gmail.com>
2021-01-12 08:32:54 +0100

    formatting

--- src/co2plot.R
+++ src/co2plot.R
@@ -26,24 +26,28 @@ data1 <- data %>%
 selection <- data1 %>%
   filter(between(time, start_am, end_pm))
 
-co2_plot <- selection %>% ggplot(aes(x = time)) + geom_line(aes(y=co2)) +
+co2_plot <-
+  ggplot(selection, aes(x = time)) +
+  geom_line(aes(y = co2)) +
   scale_x_datetime(breaks = "2 hour", date_labels = "%R") +
-  geom_hline(yintercept = 900, color = "orange", size=1.5, linetype="dotted") +
-  geom_hline(yintercept = 1200, color = "red", size=1.5, linetype="dotted") +
-  labs(y="CO2 (ppm)")
+  geom_hline(yintercept = 900, color = "orange", size = 1.5, linetype = "dotted") +
+  geom_hline(yintercept = 1200, color = "red", size = 1.5, linetype = "dotted") +
+  labs(y = "CO2 (ppm)")
 
 co2_plot <- co2_plot
 
-temp_plot <- selection %>% ggplot(aes(x = time)) + geom_line(aes(y=temperature)) +
+temp_plot <-
+  ggplot(selection, aes(x = time)) +
+  geom_line(aes(y = temperature)) +
   scale_x_datetime(breaks = "2 hour", date_labels = "%R") +
-  labs(y="Temperature (°C)")
+  labs(y = "Temperature (°C)")
 
 suppressMessages(
   co2_plot <- co2_plot +
-    ggtitle(paste(toupper(substring(who,1,1)), "-", stamp_date("Tue Jan 1, 1999")(day))) +
+    ggtitle(paste(toupper(substring(who, 1, 1)), "-", stamp_date("Tue Jan 1, 1999")(day))) +
     theme(plot.title = element_text(hjust = 0.5))
 )
-joined <- wrap_plots(co2_plot, temp_plot, ncol = 1, heights=c(4,1))
+joined <- wrap_plots(co2_plot, temp_plot, ncol = 1, heights = c(4, 1))
 
 ggsave(paste(par$output, who, "-", day, ".", par$format, sep=""), joined, height = 6, width = 8)
 
